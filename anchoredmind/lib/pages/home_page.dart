4c38f721884373ea942cf12d4559af0d17bab675
bahanley <benhanley321@gmail.com>
2025-01-20 17:02:43 +0000

    menu grid init

--- anchoredmind/lib/pages/home_page.dart
+++ anchoredmind/lib/pages/home_page.dart
@@ -11,22 +11,69 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF306468),
-        appBar: AppBar(
-          leading: BackButton(),
-          title: Row(
-            children: <Widget>[
-              Text(
-                "AnchoredMind                    ",
-                textAlign: TextAlign.center,
-              ),
-              Image.asset("images/logo_clear.png", width: 50, height: 50)
-            ],
+      backgroundColor: Color(0xFF306468),
+      appBar: AppBar(
+        leading: BackButton(),
+        title: Row(
+          children: <Widget>[
+            Text(
+              "AnchoredMind                    ",
+              textAlign: TextAlign.center,
+            ),
+            Image.asset("images/logo_clear.png", width: 50, height: 50)
+          ],
+        ),
+        backgroundColor: Color(0xFFC2F1C8),
+      ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[50],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text("Anchor"),
           ),
-          backgroundColor: Color(0xFFC2F1C8),
-        )
-
-        
-      );
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text("Journal"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[200],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text("Affirm"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[300],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text("Mindful"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.teal[400],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Text("Breath"),
+          ),
+        ],
+      )
+    );
   }
 }
