9d17e6ea9492a215b3f4a40679fabffa958cdbfc
bahanley <benhanley321@gmail.com>
2025-01-23 14:06:28 +0000

    menu with grid and icons

--- anchoredmind/lib/pages/home_page.dart
+++ anchoredmind/lib/pages/home_page.dart
@@ -25,55 +25,128 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Color(0xFFC2F1C8),
       ),
-      body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.teal[50],
-              borderRadius: BorderRadius.circular(20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 40.0), // Vertical padding
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 110, vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.teal[100],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(children: [
+                Text(
+                  'Feel',
+                  style: TextStyle(
+                    fontSize: 32.0, // Make the text larger
+                  ),
+                ),
+                Image.asset("images/logo_clear.png", width: 150, height: 150),
+              ]),
             ),
-            child: Text("Anchor"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.teal[100],
-              borderRadius: BorderRadius.circular(20),
+            Flexible(
+              // Ensures the GridView takes up the remaining space
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(10),
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+                crossAxisCount: 2,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.teal[200],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Journal',
+                          style: TextStyle(
+                            fontSize: 32.0, // Make the text larger
+                          ),
+                        ),
+                        Icon(
+                          Icons.edit_document, // The icon to display
+                          size: 70, // Optional: size of the icon
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.teal[200],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Gratitude',
+                          style: TextStyle(
+                            fontSize: 32.0, // Make the text larger
+                          ),
+                        ),
+                        Icon(
+                          Icons.health_and_safety, // The icon to display
+                          size: 70, // Optional: size of the icon
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.teal[300],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Mindful',
+                          style: TextStyle(
+                            fontSize: 32.0, // Make the text larger
+                          ),
+                        ),
+                        Icon(
+                          Icons
+                              .self_improvement_outlined, // The icon to display
+                          size: 70, // Optional: size of the icon
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.teal[300],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Breath',
+                          style: TextStyle(
+                            fontSize: 32.0, // Make the text larger
+                          ),
+                        ),
+                        Icon(
+                          Icons.air, // The icon to display
+                          size: 70, // Optional: size of the icon
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Text("Journal"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.teal[200],
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Text("Affirm"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.teal[300],
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Text("Mindful"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.teal[400],
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: const Text("Breath"),
-          ),
-        ],
-      )
+          ],
+        ),
+      ),
     );
   }
 }
