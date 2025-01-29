21090dab8cd21b932e782afd973ce0bd2687965c
bahanley <benhanley321@gmail.com>
2025-01-29 16:19:32 +0000

    implement menu class

--- anchoredmind/lib/pages/home_page.dart
+++ anchoredmind/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import '../components/menu_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,6 +31,7 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: [
+            //Todo width 2 MenuCard
             Container(
               padding:
                   const EdgeInsets.symmetric(horizontal: 110, vertical: 20),
@@ -56,91 +58,10 @@ class _HomePageState extends State<HomePage> {
                 mainAxisSpacing: 15,
                 crossAxisCount: 2,
                 children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.teal[200],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Journal',
-                          style: TextStyle(
-                            fontSize: 32.0, // Make the text larger
-                          ),
-                        ),
-                        Icon(
-                          Icons.edit_document, // The icon to display
-                          size: 70, // Optional: size of the icon
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.teal[200],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Gratitude',
-                          style: TextStyle(
-                            fontSize: 32.0, // Make the text larger
-                          ),
-                        ),
-                        Icon(
-                          Icons.health_and_safety, // The icon to display
-                          size: 70, // Optional: size of the icon
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.teal[300],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Mindful',
-                          style: TextStyle(
-                            fontSize: 32.0, // Make the text larger
-                          ),
-                        ),
-                        Icon(
-                          Icons
-                              .self_improvement_outlined, // The icon to display
-                          size: 70, // Optional: size of the icon
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.teal[300],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Breath',
-                          style: TextStyle(
-                            fontSize: 32.0, // Make the text larger
-                          ),
-                        ),
-                        Icon(
-                          Icons.air, // The icon to display
-                          size: 70, // Optional: size of the icon
-                        ),
-                      ],
-                    ),
-                  ),
+                  MenuCard(name: "Journal", icon: Icons.edit_document),
+                  MenuCard(name: "Gratitude", icon: Icons.health_and_safety),
+                  MenuCard(name: "Mindful", icon: Icons.self_improvement, colour: Color(0xFF4DB6AC)),
+                  MenuCard(name: "Breath", icon: Icons.air, colour: Color(0xFF4DB6AC)),
                 ],
               ),
             ),
