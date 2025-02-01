c116f701639b57e2ababb6bcd1be3ebeb7c4ea83
bahanley <benhanley321@gmail.com>
2025-02-01 22:55:44 +0000

    sliding slider

--- anchoredmind/lib/components/my_audio_player.dart
+++ anchoredmind/lib/components/my_audio_player.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import '../components/my_colours.dart';
 
-class MyAudioPlayer extends StatelessWidget {
-  // Constructor
-  const MyAudioPlayer({
-    super.key,
-  });
+class MyAudioPlayer extends StatefulWidget {
+  const MyAudioPlayer({super.key});
+
+  @override
+  State<MyAudioPlayer> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<MyAudioPlayer> {
+  double _currentSliderValue = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +28,17 @@ class MyAudioPlayer extends StatelessWidget {
             Icon(Icons.forward_10, size: 40),
           ]),
           Slider(
-            value: 50,
+            value: _currentSliderValue,
             max: 100,
-            divisions:100,
-            label: "Slider",
+            divisions: 100,
+            label: _currentSliderValue.round().toString(),
             thumbColor: MyColours.backgroundGreen,
             activeColor: MyColours.darkTeal,
             inactiveColor: MyColours.black,
             onChanged: (double value) {
-              
+              setState(() {
+                _currentSliderValue = value;
+              });
             },
           ),
         ]),
