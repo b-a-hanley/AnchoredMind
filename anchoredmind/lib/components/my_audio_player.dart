64447011aaf007223b3b31569879b51fabcd7cc3
bahanley <benhanley321@gmail.com>
2025-02-01 18:19:15 +0000

    audioplayer init

---
+++ anchoredmind/lib/components/my_audio_player.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../components/my_colours.dart';
+
+class MyAudioPlayer extends StatelessWidget {
+  // Constructor
+  const MyAudioPlayer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: MyColours.teal,
+          borderRadius: BorderRadius.circular(32),
+        ),
+        child: Column(children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            Icon(Icons.replay_10, size: 40),
+            Icon(Icons.play_circle, size: 40),
+            Icon(Icons.forward_10, size: 40),
+          ]),
+          Slider(
+            value: 0,
+            max: 100,
+            divisions:100,
+            label: "Slider",
+            thumbColor: MyColours.backgroundGreen,
+            activeColor: MyColours.lightTeal,
+            onChanged: (double value) {
+              
+            },
+          ),
+        ]),
+      ),
+    );
+  }
+}
