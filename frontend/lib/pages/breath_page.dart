6a5e2cd336f38b5eb3b3a8b31f4460f414befd65
bahanley <benhanley321@gmail.com>
2025-03-19 23:09:50 +0000

    breath page

--- frontend/lib/pages/breath_page.dart
+++ frontend/lib/pages/breath_page.dart
@@ -3,24 +3,38 @@ import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+  
+class BreathPage extends StatefulWidget {
+  @override
+  _BreathPageState createState() => _BreathPageState();
+}
 
-class BreathPage extends StatelessWidget {
-  BreathPage({super.key});
+class _BreathPageState extends State<BreathPage> {
+  late FlickManager flickManager;
 
-  final FlickManager flickManager = FlickManager(
-    videoPlayerController: VideoPlayerController.asset("assets/videos/test1.mp4"),
-  );
+  @override
+  void initState() {
+    super.initState();
+    flickManager = FlickManager(
+      videoPlayerController: VideoPlayerController.asset(
+        "assets/videos/breath.mp4",
+      )..setLooping(true),
+    );
+  }
+
+  @override
+  void dispose() {
+    flickManager.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(context, name: AppLocalizations.of(context)!.breath),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 9 / 16,
-          child: FlickVideoPlayer(flickManager: flickManager),
+        appBar: MyAppBar(context, name: AppLocalizations.of(context)!.breath),
+        body: Expanded(
+          child:  FlickVideoPlayer(flickManager: flickManager),
         ),
-      ),
-    );
+      );
   }
 }
