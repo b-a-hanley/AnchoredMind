01a90bf2fa3b71389146cb046b3da4a1a8780223
bahanley <benhanley321@gmail.com>
2025-04-14 23:48:14 +0100

    heartrate device

--- frontend/lib/pages/profile_page.dart
+++ frontend/lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
-
+import 'dart:async';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:frontend/services/heartrate_service.dart';
 import '../components/my_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -7,27 +8,54 @@ import '../providers/localeprovider.dart';
 import '../components/my_colours.dart';
 
 class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
   @override
   ProfilePageState createState() => ProfilePageState();
 }
 
 class ProfilePageState extends State<ProfilePage> {
-  HeartrateService heartrateService = HeartrateService();
+  final HeartrateService heartrateService = HeartrateService();
   String? selectedDevice;
-  late Stream<List<String>> scan;
-  
+  List<String> deviceNames = [];
+  bool isScanning = true;
+  StreamSubscription? scanSubscription;
+
   @override
   void initState() {
-    //HeartRateService.instance.scan();
-    scan = heartrateService.scan();
     super.initState();
+    startScan();
   }
 
-  Future<List<DropdownMenuEntry<String>>> getDeviceDropdownEntries() async {
-    final names = await heartrateService.scan().first; // Just wait for first result
-    return names.map((name) => DropdownMenuEntry(value: name, label: name)).toList();
+  @override
+  void dispose() {
+    heartrateService.stopScan();
+    super.dispose();
   }
 
+  void startScan() {
+    setState(() {
+      deviceNames = [];
+    });
+
+    scanSubscription = heartrateService.scan().listen((devices) {
+      setState(() {
+        deviceNames = devices.where((name) => name.isNotEmpty).toList();
+      });
+    }, onDone: () {
+      setState(() {
+        isScanning = false;
+      });
+    });
+  }
+
+  List<DropdownMenuEntry<String>> getDeviceDropdownEntries() {
+    return deviceNames
+        .map((name) => DropdownMenuEntry(value: name, label: name))
+        .toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final localeProvider = Provider.of<LocaleProvider>(context);
 
@@ -38,85 +66,77 @@ class ProfilePageState extends State<ProfilePage> {
         margin: EdgeInsets.all(15),
         padding: EdgeInsets.symmetric(horizontal: 15),
         decoration: BoxDecoration(
-        color: MyColours.teal,
-        borderRadius: BorderRadius.circular(15),
+          color: MyColours.teal,
+          borderRadius: BorderRadius.circular(15),
         ),
         child: ConstrainedBox(
-        constraints: const BoxConstraints.expand(),
-        child: Column(
-          children: [
-            Text(
-              'Language',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 24.0,
-              )
-            ),
-            DropdownButton<Locale>(
-              value: localeProvider.locale,
-
-              onChanged: (Locale? newLocale) {
-                if (newLocale != null) {
-                  localeProvider.setLocale(newLocale);
-                }
-              },
-              items: [
-                DropdownMenuItem(
-                  value: Locale('en'),
-                  child: Text('English'),
-                ),
-                DropdownMenuItem(
-                  value: Locale('ar'),
-                  child: Text('العربية'),
-                ),
-                DropdownMenuItem(
-                  value: Locale('hi'),
-                  child: Text('हिन्दी'),
-                ),
-                DropdownMenuItem(
-                  value: Locale('bn'),
-                  child: Text('বাংলা'),
-                ),
-                DropdownMenuItem(
-                  value: Locale('zh'),
-                  child: Text('繁體'),
-                ),
-              ],
-            ),
-            Text(
-                'Heartrate Devices',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 24.0,
-                )
-            ),
-            StreamBuilder<List<String>>(
-                stream: scan,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                    final deviceNames = snapshot.data!;
-                    return DropdownButton<String>(
-                      value: selectedDevice,
-                      hint: Text('Select a Device'),
-                      onChanged: (String? newDevice) {
-                        setState(() {
-                          selectedDevice = newDevice;
-                        });
-                      },
-                      items: deviceNames
-                          .map((deviceName) => DropdownMenuItem<String>(
-                                value: deviceName,
-                                child: Text(deviceName),
-                              ))
-                          .toList(),
-                    );
-                  } else {
-                    return Text('No devices found');
+          constraints: BoxConstraints.expand(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Language',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              DropdownButton<Locale>(
+                value: localeProvider.locale,
+                onChanged: (Locale? newLocale) {
+                  if (newLocale != null) {
+                    localeProvider.setLocale(newLocale);
                   }
                 },
+                items: [
+                  DropdownMenuItem(
+                    value: Locale('en'),
+                    child: Text('English'),
+                  ),
+                  DropdownMenuItem(
+                    value: Locale('ar'),
+                    child: Text('العربية'),
+                  ),
+                  DropdownMenuItem(
+                    value: Locale('hi'),
+                    child: Text('हिन्दी'),
+                  ),
+                  DropdownMenuItem(
+                    value: Locale('bn'),
+                    child: Text('বাংলা'),
+                  ),
+                  DropdownMenuItem(
+                    value: Locale('zh'),
+                    child: Text('繁體'),
+                  ),
+                ],
               ),
+              SizedBox(height: 20),
+              Text(
+                'Heartrate Devices',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              SizedBox(height: 10),
+              deviceNames.isEmpty
+                ? Center(child: CircularProgressIndicator())
+                : DropdownMenu<String>(
+                    label: Text("Select Heartrate device"),
+                    initialSelection: selectedDevice,
+                    onSelected: (String? newDevice) {
+                      setState(() {
+                        selectedDevice = newDevice;
+                      });
+                      heartrateService.stopScan();
+                      if (newDevice != null) {
+                        heartrateService
+                            .connectToDevice(newDevice)
+                            .then((result) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(result)),
+                          );
+                        });
+                      }
+                    },
+                    dropdownMenuEntries: getDeviceDropdownEntries(),
+                  ),
             ],
           ),
         ),
--- frontend/lib/services/heartrate_service.dart
+++ frontend/lib/services/heartrate_service.dart
@@ -1,44 +1,46 @@
 import 'dart:async';
+
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class HeartrateService {
   final Guid heartRateServiceUUID = Guid("0000180D-0000-1000-8000-00805F9B34FB");
-  Stream<List<ScanResult>> scanResults = FlutterBluePlus.scanResults;
   
   Stream<List<String>> scan() {
     FlutterBluePlus.startScan(
-      timeout: Duration(seconds: 10),
+      timeout: const Duration(seconds: 10),
+      androidUsesFineLocation: true,
+      withServices: [heartRateServiceUUID]
     );
-
-    scanResults = FlutterBluePlus.scanResults;
+    
     return FlutterBluePlus.scanResults.map(
-      (results) => results.map((result) => result.advertisementData.advName).toList(),
+      (results) => results
+          .map((result) => result.device.platformName)
+          .where((name) => name.isNotEmpty)
+          .toSet()
+          .toList(),
     );
   }
 
-  Future<String> connectToDevice(String? device) async {
+  Future<String> connectToDevice(String device) async {
     final completer = Completer<String>();
-
-    FlutterBluePlus.scanResults.listen((results) async {
-      for (ScanResult r in results) {
-        if (r.advertisementData.advName == device) {
+    
+    StreamSubscription? subscription;
+    subscription = FlutterBluePlus.scanResults.listen((results) async {
+      for (ScanResult result in results) {
+        if (result.device.platformName == device) {
           await FlutterBluePlus.stopScan();
-          await r.device.connect();
-          completer.complete("Connected to ${r.device.platformName}");
+          await result.device.connect();
+          completer.complete("Connected to ${result.device.platformName}");
+          subscription?.cancel();
           break;
         }
       }
     });
-
+    
     return completer.future;
   }
 
-  // void readHeartRateData(BluetoothCharacteristic characteristic) async {
-  //   // Start listening to the characteristic to get heart rate data
-  //   characteristic.setNotifyValue(true);
-  //   characteristic.lastValueStream.listen((value) {
-  //     // Parse the heart rate data from the value
-  //     print('Heart Rate Data: $value');
-  //   });
-  // }
+  void stopScan() {
+    FlutterBluePlus.stopScan();
+  }
 }
