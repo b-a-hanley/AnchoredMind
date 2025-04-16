27d442c35abca317b2bc773cc6404608b5435d85
bahanley <benhanley321@gmail.com>
2025-04-16 13:47:23 +0100

    request permissions

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,23 +1,35 @@
+import 'package:frontend/pages/home_page.dart';
+import 'package:frontend/pages/login_page.dart';
 import 'languages/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart'; 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/localeprovider.dart';
-import 'pages/home_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'services/local_db_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 late LocalDBService localDbService;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocalDBService.instance.init();
+  requestPermissions();
   runApp(
     ChangeNotifierProvider(
       create: (context) => LocaleProvider(),
       child: AnchoredMind(),
     ),
   );
+}
+
+Future<void> requestPermissions() async {
+  await [
+    Permission.bluetooth,
+    Permission.bluetoothConnect,
+    Permission.bluetoothScan,
+    Permission.locationWhenInUse,
+  ].request();
 }
 
 class AnchoredMind extends StatelessWidget {
